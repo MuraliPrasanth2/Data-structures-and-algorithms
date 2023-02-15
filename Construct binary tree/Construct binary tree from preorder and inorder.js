99b8cd38f4712038761ab14a77d40095f6e61d92
Murali Prasanth <mprasanth18@gmail.com>
2023-02-15 16:01:00 +0530

    Construct binary tree from preorder and inorder

--- Construct binary tree/Construct binary tree from preorder and inorder.js
+++ Construct binary tree/Construct binary tree from preorder and inorder.js
@@ -6,7 +6,7 @@ class Node {
     }
 }
 
-// recursive less optimized.
+// recursive less optimized build tree from preorder and postOrder
 var buildTree = function (preorder, inorder, preorderStart = 0, preorderEnd = preorder.length - 1, inorderStart = 0, inorderEnd = inorder.length - 1) {
     if (preorderStart > preorderEnd) return null;
 
@@ -23,6 +23,31 @@ var buildTree = function (preorder, inorder, preorderStart = 0, preorderEnd = pr
 }
 
 
+
+// recursive more optimized build tree from preorder and postOrder
+var buildTree = function (preorder, inorder) {
+
+    const inorderIndexHasMap = {};
+    inorder.forEach((item, index) => inorderIndexHasMap[item] = index);
+
+    function treeFromPreorderArray(preorderStart, preorderEnd, inorderStart) {
+        const currentNodeValue = preorder[preorderStart];
+        const currentNode = new Node(currentNodeValue);
+
+        const inorderMid = inorderIndexHasMap[currentNodeValue];
+        const numItemsBeforeMid = inorderMid - inorderStart;
+
+        if (preorderStart + 1 <= preorderStart + numItemsBeforeMid)
+            currentNode.left = treeFromPreorderArray(preorderStart + 1, preorderStart + numItemsBeforeMid, inorderStart);
+        if (preorderStart + numItemsBeforeMid + 1 <= preorderEnd)
+            currentNode.right = treeFromPreorderArray(preorderStart + numItemsBeforeMid + 1, preorderEnd, inorderMid + 1);
+        return currentNode;
+    }
+    // if (preorderStart > preorderEnd) return null;
+    return treeFromPreorderArray(0, preorder.length - 1, 0);
+}
+
+
 console.log(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]));
-// console.log(buildTree([-1], [-1]));
-// console.log(buildTree([1, 2, 3], [3, 2, 1]));
+console.log(buildTree([-1], [-1]));
+console.log(buildTree([1, 2, 3], [3, 2, 1]));
